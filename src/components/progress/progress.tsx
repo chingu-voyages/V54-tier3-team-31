9f664f187aa55e91c6f2906b0a084f532d0bd927
Jericho Serrano <118679068+jericho1050@users.noreply.github.com>
2025-03-27 22:17:41 +0800

    chore: remove navbars in progress page

--- src/components/progress/progress.tsx
+++ src/components/progress/progress.tsx
@@ -1,23 +1,11 @@
 'use client'
 
 // import { useState } from 'react'
-import NavigationMenu from '../navigation/navigation-menu'
-import DesktopNav from '../navigation/desktop-nav'
-
 const Progress = () => {
     return (
         <div className="min-h-screen flex flex-col">
-            {/* Desktop Navigation */}
-            <DesktopNav />
-
-            {/* Main Container */}
             <div>
                 <h2>Progress Page</h2>
-            </div>
-
-            {/*Mobile-only Navigation*/}
-            <div className="md:hidden">
-                <NavigationMenu />
             </div>
         </div>
     )
